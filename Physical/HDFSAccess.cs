d9b9cd65d697a1f864714cce2a56ea2378acbb3b
Echo Lee <bdottn.lee+git@gmail.com>
2017-08-15 16:20:37 +0800

    建立 HDFSAccess
    1.　加入建構式：HDFSAccess
    　1.1　uriString：HDFS 進入點
    　1.2　userName：帳號
    
    2.　加入方法：GetDirectories
    
    3.　加入方法：GetFiles

---
+++ Physical/HDFSAccess.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Hadoop.WebHDFS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Physical
+{
+    public sealed class HDFSAccess
+    {
+        private readonly WebHDFSClient webHDFSClient;
+
+        public HDFSAccess(string uriString, string userName)
+        {
+            this.webHDFSClient = new WebHDFSClient(new Uri(uriString), userName);
+        }
+
+        public List<string> GetDirectories(string path)
+        {
+            var directoryStatus = this.webHDFSClient.GetDirectoryStatus(path).Result;
+
+            return directoryStatus.Directories.Select(d => d.PathSuffix).ToList();
+        }
+
+        public List<string> GetFiles(string path)
+        {
+            var directoryStatus = this.webHDFSClient.GetDirectoryStatus(path).Result;
+
+            return directoryStatus.Files.Select(d => d.PathSuffix).ToList();
+        }
+    }
+}
