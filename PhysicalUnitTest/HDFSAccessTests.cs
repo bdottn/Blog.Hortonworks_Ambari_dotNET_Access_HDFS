5db0e8f263c433b68aa8d099fa95079a3926d5b9
Echo Lee <bdottn.lee+git@gmail.com>
2017-08-15 16:24:35 +0800

    建立 HDFSAccessTests 單元測試
    1.　加入測試案例：GetDirectoriesTest_傳入根目錄_預期回傳預設目錄
    2.　加入測試案例：GetFilesTest_傳入根目錄_預期回傳空集合

---
+++ PhysicalUnitTest/HDFSAccessTests.cs
@@ -0,0 +1,48 @@
+﻿using ExpectedObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Physical.UnitTest
+{
+    [TestClass]
+    public sealed class HDFSAccessTests
+    {
+        private HDFSAccess access;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            // HDFS cluster 客戶端進入端點設定於 AMBARI.LAB 主機上
+            // 預設端點：http://[Ambari 主機名稱]:50070
+            // 預設帳號：hdfs
+            this.access = new HDFSAccess(@"http://AMBARI.LAB:50070", "hdfs");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+        }
+
+        [TestMethod]
+        public void GetDirectoriesTest_傳入根目錄_預期回傳預設目錄()
+        {
+            // 預設根目錄下有兩個目錄：tmp、user
+            var expected = new List<string>() { "tmp", "user", };
+
+            var actual = this.access.GetDirectories(@"/");
+
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+
+        [TestMethod]
+        public void GetFilesTest_傳入根目錄_預期回傳空集合()
+        {
+            // 預設根目錄下沒有檔案
+            var expected = new List<string>();
+
+            var actual = this.access.GetFiles(@"/");
+
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+    }
+}
