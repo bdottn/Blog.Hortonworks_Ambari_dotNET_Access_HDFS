a67896381fa0f1536922c8f5056f0b3ffd75eebb
Echo Lee <bdottn.lee+git@gmail.com>
2017-08-15 16:27:40 +0800

    調整 HDFSAccessTests 單元測試
    1.　加入測試案例：DirectoryTest_建立zzz目錄_預期成功_預期根目錄下有zzz目錄_刪除zzz目錄_預期成功_預期根目錄下無zzz目錄

--- PhysicalUnitTest/HDFSAccessTests.cs
+++ PhysicalUnitTest/HDFSAccessTests.cs
@@ -21,6 +21,21 @@ namespace Physical.UnitTest
         [TestCleanup]
         public void TestCleanup()
         {
+            // 取得根目錄資料夾
+            var directories = this.access.GetDirectories(@"/");
+
+            // 移除預設目錄：tmp、user 外的目錄
+            foreach (var directory in directories)
+            {
+                if ("tmp".Equals(directory) || "user".Equals(directory))
+                {
+                    continue;
+                }
+                else
+                {
+                    this.access.DeleteDirectory(directory);
+                }
+            }
         }
 
         [TestMethod]
@@ -43,6 +58,40 @@ namespace Physical.UnitTest
             var actual = this.access.GetFiles(@"/");
 
             expected.ToExpectedObject().ShouldEqual(actual);
+        }
+
+        [TestMethod]
+        public void DirectoryTest_建立zzz目錄_預期成功_預期根目錄下有zzz目錄_刪除zzz目錄_預期成功_預期根目錄下無zzz目錄()
+        {
+            var directoryName = "zzz";
+
+            // 建立zzz目錄
+            var boolCreateDirectory = this.access.CreateDirectory(directoryName);
+
+            // 建立zzz目錄_預期成功
+            Assert.IsTrue(boolCreateDirectory);
+
+            // 建立zzz目錄_預期成功_預期根目錄下有zzz目錄
+            // 預設根目錄下有三個目錄：tmp、user、zzz
+            var expectedCreateDirectory = new List<string>() { "tmp", "user", directoryName, };
+
+            var actualCreateDirectory = this.access.GetDirectories(@"/");
+
+            expectedCreateDirectory.ToExpectedObject().ShouldEqual(actualCreateDirectory);
+
+            // 刪除zzz目錄
+            var boolDeleteDirectory = this.access.DeleteDirectory(directoryName);
+
+            // 刪除zzz目錄_預期成功
+            Assert.IsTrue(boolCreateDirectory);
+
+            // 刪除zzz目錄_預期成功_預期根目錄下無zzz目錄
+            // 預設根目錄下有兩個目錄：tmp、user
+            var expectedDeleteDirectory = new List<string>() { "tmp", "user", };
+
+            var actualDeleteDirectory = this.access.GetDirectories(@"/");
+
+            expectedDeleteDirectory.ToExpectedObject().ShouldEqual(actualDeleteDirectory);
         }
     }
 }
